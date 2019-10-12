92759bb4c05bacf4b0d45a8ff9db5e1705ac5c11
jelleyz <sadiq.registrations@outlook.com>
2019-10-12 19:39:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ky5ze9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ky5ze9)
---
+++ index.js
@@ -0,0 +1,133 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
+
+class BB extends Component {
+  constructor() {
+    console.log("constructor");
+    this.state = {
+      name: "State"
+    };
+  }
+
+  render() {
+    console.log("render");
+
+    return (
+      <div>
+        <h1>BB </h1>
+        <h2>Props: {this.props.name}</h2>
+        <h2>State: {this.state.name}</h2>
+        <button onClick={e => this.setState({ name: this.state.name + 1 })}>
+          Change State
+        </button>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(
+      "componentDidUpdate",
+      "prevProps",
+      prevProps,
+      "props",
+      this.props,
+      "prevState",
+      prevState,
+      "state",
+      this.state,
+      "snapshot",
+      snapshot
+    );
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(
+      "shouldComponentUpdate",
+      "nextProps",
+      nextProps,
+      "nextState",
+      nextState
+    );
+    return true;
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log(
+      "getDerivedStateFromProps",
+      // "this.props",
+      // this.props,
+      // "this.state",
+      // this.state,
+      "props",
+      props,
+      "state",
+      state
+    );
+    return null;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log(
+      "getSnapshotBeforeUpdate",
+      "prevProps",
+      prevProps,
+      "props",
+      this.props,
+      "prevState",
+      prevState,
+      "state",
+      this.state
+    );
+    return null;
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Prop",
+      mount: true,
+      count: 1
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.mount && <BB name={this.state.name} />}
+        <button
+          onClick={e =>
+            this.setState({
+              name: this.state.name + " " + this.state.count,
+              count: this.state.count + 1
+            })
+          }
+        >
+          Chnage Props
+        </button>
+        <button
+          onClick={e =>
+            this.setState({
+              mount: !this.state.mount
+            })
+          }
+        >
+          Toggle Mount
+        </button>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
